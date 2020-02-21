7173f984967ba418332640699b54632d873aac0c
SymeonChen <symeonchen@gmail.com>
2020-02-21 21:37:55 +0800

    Add xml for bright time setting page.

---
+++ app/src/main/java/com/symeonchen/wakeupscreen/pages/WakeUptimeSettingActivity.kt
@@ -0,0 +1,17 @@
+package com.symeonchen.wakeupscreen.pages
+
+import android.os.Bundle
+import com.symeonchen.wakeupscreen.ScBaseActivity
+
+
+/**
+ * Created by SymeonChen on 2020-02-20.
+ */
+class WakeUptimeSettingActivity : ScBaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
